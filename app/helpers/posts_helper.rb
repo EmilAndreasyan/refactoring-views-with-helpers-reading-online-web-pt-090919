31fcf9e8518e2284f45d956d3719a280cf1c0119
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-02-16 02:22:35 +0000

    Automatically backed up by Learn

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,7 @@
 module PostsHelper
   
-  def last_updated
-    updated_at.strftime("Last updated %A, %b %e, at 1%:%M %p")
+  def last_updated(post)
+    [post.updated_at.strftime("Last updated %A, %b %e, at 1%:%M %p")
   end
 
 end
